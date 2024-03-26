fd9c38caf50ac9e8a6d8f2b016687e98f107178a
kmarilley001 <kmarilley001@student.franciscan.edu>
2024-03-26 12:18:27 -0400

    session 17

--- src/m1_scope.py
+++ src/m1_scope.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (9 pts)
+# DONE: 1. (9 pts)
 #
 #   For this _todo_, reference the code below.
 #
@@ -49,6 +49,7 @@ def foo(a):
     return t1.m
 
 
+
 def bar(a):
     ### Location 6
     m = 1
@@ -57,44 +58,48 @@ def bar(a):
     ### Location 7
     return a
 
+
 def main():
     ### Location 8
-    a = foo(a)
+    a1 = foo(a)
     m = bar(a)
     ### Location 9
+    print(m)
+
+
 
 main()
 
 ###############################################################################
 # 
 #   Location 1          Location 2          Location 3
-#   a                   a                   a               
-#   m                   m                   m               
-#   self.a              self.a              self.a          
-#   self.m              self.m              self.m          
-#   t1.a                t1.a                t1.a            
-#   t1.m                t1.m                t1.m            
-#   t2.a                t2.a                t2.a            
-#   t2.m                t2.m                t2.m            
+#   a        10       a      10             a           20     
+#   m        X        m       X             m           X    
+#   self.a   X        self.a  5            self.a       X
+#   self.m   X        self.m  13           self.m       X 
+#   t1.a     X        t1.a     X           t1.a         X  
+#   t1.m     X        t1.m     X           t1.m         X 
+#   t2.a     X        t2.a     X           t2.a         X  
+#   t2.m     X        t2.m     X           t2.m         X 
 #
 #   Location 4          Location 5          Location 6
-#   a                   a                   a               
-#   m                   m                   m               
-#   self.a              self.a              self.a          
-#   self.m              self.m              self.m          
-#   t1.a                t1.a                t1.a            
-#   t1.m                t1.m                t1.m            
-#   t2.a                t2.a                t2.a            
-#   t2.m                t2.m                t2.m            
+#   a         34         a      34          a     28          
+#   m         25         m      25          m      X         
+#   self.a    X         self.a   X         self.a  X        
+#   self.m    X         self.m   X         self.m   X   
+#   t1.a      X         t1.a     5         t1.a     X       
+#   t1.m      X         t1.m     28        t1.m     X         
+#   t2.a      X         t2.a     5         t2.a     X        
+#   t2.m      X         t2.m     15        t2.m     X         
 #
 #   Location 7          Location 8          Location 9
-#   a                   a                   a               
-#   m                   m                   m               
-#   self.a              self.a              self.a          
-#   self.m              self.m              self.m          
-#   t1.a                t1.a                t1.a            
-#   t1.m                t1.m                t1.m            
-#   t2.a                t2.a                t2.a            
-#   t2.m                t2.m                t2.m            
+#   a           29      a        20           a      28        
+#   m           3       m         X           m      29      
+#   self.a      X       self.a    X          self.a   X      
+#   self.m      X       self.m    X          self.m   X       
+#   t1.a        X       t1.a      X           t1.a    X     
+#   t1.m        X       t1.m      X          t1.m     X       
+#   t2.a        X       t2.a      X          t2.a     X       
+#   t2.m        X       t2.m      X          t2.m     X       
 #
 ###############################################################################
--- src/m2_inheritance.py
+++ src/m2_inheritance.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (4 pts)
+# DONE: 1. (4 pts)
 #
 #   For this module, we are going to build off our Pet class that we created in
 #   m2 of the session 16 coding exercises.
@@ -26,9 +26,25 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+class Pet: 
+    def __init__(self,name,age):
+        self.name = name 
+        self.age = age   
+
+    def __str__(self): 
+        return f"Name: {self.name}, Age: {self.age}"
+    
+    def speak(self): 
+        print(f"{self.name}: Bark! Bark! Bark!")
+
+class Dog(Pet):
+    def fetch(self): 
+        print(f"{self.name} ran to get the stick!")
+    def speak(self): 
+        print(f"{self.name}: Bark! Bark! Bark!")
 
 ###############################################################################
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, write your own class of whatever type of pet you wish.
 #
@@ -46,7 +62,35 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+class Pet: 
+    def __init__(self,name,age):
+        self.name = name 
+        self.age = age   
 
+    def __str__(self): 
+        return f"Name: {self.name}, Age: {self.age}"
+    
+    def speak(self): 
+        print(f"{self.name}: Bark! Bark! Bark!")
+
+class Dog(Pet):
+    def fetch(self): 
+        print(f"{self.name} ran to get the stick!")
+    def speak(self): 
+        print(f"{self.name}: Bark! Bark! Bark!")
+
+class Fish(Pet): 
+    def __init__(self,name,age,color,species):
+        super().__init__(name,age) 
+        self.color = color
+        self.species= species
+    def swim(self): 
+        print(f"{self.name} is a {self.species} fish")
+    def describe(self): 
+        print(f"{self.name} is a {self.color} fish and is {self.age}")
+
+
+       
 ###############################################################################
 # TODO: 3. (4 pts)
 #
@@ -71,4 +115,20 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+def main(): 
+    my_dog = Dog("Misty", 10)
+    my_fish = Fish("Bubbles", 3,"Blue", "Beta")
+
+    print(my_dog)
+    print(my_fish)
+
+    my_dog.speak()
+    my_fish.speak()
+    my_dog.fetch()
+    my_fish.swim()
+    my_dog.speak()
+    my_fish.describe()
+
+main()
+
 
